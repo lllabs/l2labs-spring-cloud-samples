2171427b3a5e3032e4d481c07579586535d82542
razy <razy.dev@spiderkim.com>
2020-11-25 19:11:55 +0900

    working ...

---
+++ oauth2-server-jwt/src/main/java/samples/config/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package samples.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("razy").password("{noop}111").roles("USER").authorities("ALL");
+    }
+
+
+}
---
+++ resource-oauth2-sso/src/main/java/sping/cloud/samples/SampleController.java
@@ -0,0 +1,16 @@
+package sping.cloud.samples;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableOAuth2Sso
+@RestController
+public class SampleController {
+
+    @RequestMapping({"", "/"})
+    String home() {
+        return "Hello World";
+    }
+
+}
